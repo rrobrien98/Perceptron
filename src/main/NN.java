c08f68a165ab75cea446e9784ab65146f45549d7
school <school@maplieu.local>
2019-11-28 21:19:01 -0500

    init

--- src/main/NN.java
+++ src/main/NN.java
@@ -1,5 +1,22 @@
 package main;
 
 public class NN {
+	
+	//connection object
+		//wieght
+		//input node
+		//output node
+		
+	
+		// output nodes
+			// 
+	
+	
+		// update nodes
+			// calculate output values
+			// loop through output nodes
+				// through connections made to it
+					// 
+		//
 
 }
